1cb2f890ab79d0b20fc45afee09e9c004500a570
warawanaidene <waramendo@gmail.com>
2023-09-06 22:38:58 -0400

    init

---
+++ CustomInteraction.cs
@@ -0,0 +1,65 @@
+﻿using EFT;
+using EFT.InventoryLogic;
+using EFT.UI;
+using IcyClawz.CustomInteractions;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace TraderModding
+{
+
+    internal static class PlayerExtensions
+    {
+        private static readonly FieldInfo InventoryControllerField =
+            typeof(Player).GetField("_inventoryController", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        public static InventoryControllerClass GetInventoryController(this Player player) =>
+            InventoryControllerField.GetValue(player) as InventoryControllerClass;
+    }
+
+    internal sealed class CustomInteractionsProvider : IItemCustomInteractionsProvider
+    {
+        internal static StaticIcons StaticIcons => EFTHardSettings.Instance.StaticIcons;
+
+        public IEnumerable<CustomInteraction> GetCustomInteractions(ItemUiContext uiContext, EItemViewType viewType, Item item)
+        {
+            if (viewType != EItemViewType.Inventory)
+            {
+                yield break;
+            }
+            {
+                yield return new CustomInteraction()
+                {
+                    Caption = () => "TRADER MODDING",
+                    Icon = () => StaticIcons.GetAttributeIcon(EItemAttributeId.Weapon),
+                    Enabled = () => true,
+                    Action = () =>
+                    {
+                        // Singleton<GUISounds>.Instance.PlayUISound(EUISoundType.MenuContextMenu);
+                        ComponentUtils.TraderModding(uiContext, item);
+                        // uiContext.RedrawContextMenus(new[] { item.TemplateId });
+                    }
+                };
+                yield break;
+
+            }
+        }
+    }
+
+    internal static class ComponentUtils
+    {
+        public static void TraderModding(ItemUiContext uiContext, Item weapon)
+        {
+            Globals.isTraderModding = true;
+            // We grab data from server of all loyalty unlocked mods from traders that aren't a barter offer.
+            Globals.traderMods = TraderModdingUtils.GetData();
+            // add the parts from the current gun to the traderMods list because spaghetti :D
+            TraderModdingUtils.AddGunParts(weapon);
+
+            uiContext.EditBuild((Weapon)weapon);
+        }
+    }
+
+
+
+}
---
+++ TraderModdingPatch.cs
@@ -0,0 +1,58 @@
+﻿using Aki.Reflection.Patching;
+using EFT.InventoryLogic;
+using System.Reflection;
+using System.Linq;
+using EFT.UI.WeaponModding;
+using UnityEngine;
+using EFT.UI;
+using EFT.UI.Screens;
+
+namespace TraderModding
+{
+    public class ModsHidePatch : ModulePatch
+    {
+        // This patch returns false on the method that populates mods in the slot view container. pretty
+        // spaghetti but it works
+
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(DropDownMenu).GetMethod("method_0", BindingFlags.Instance | BindingFlags.NonPublic);
+        }
+
+        [PatchPrefix]
+        static bool Prefix(Item item, ref RectTransform container)
+        {
+            if (Globals.isTraderModding && Globals.traderMods.Length > 0)
+            {
+                if (!Globals.traderMods.Any(mod => mod == item.TemplateId))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    public class ScreenChangePatch : ModulePatch
+    {
+        // A patch that stops trader modding logic if player goes to inventory, menu or hideout.
+
+        // TODO eventually add a way for player to be able to go to character tab in
+        // TaskBarMenu without clearing trader modding.
+
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(MenuTaskBar).GetMethod("OnScreenChanged", BindingFlags.Instance | BindingFlags.Public);
+        }
+
+        [PatchPrefix]
+        static void Prefix(EEftScreenType eftScreenType)
+        {
+            if (eftScreenType == EEftScreenType.Inventory || eftScreenType == EEftScreenType.MainMenu || eftScreenType == EEftScreenType.Hideout)
+            {
+                TraderModdingUtils.EndTraderModding();
+            }
+        }
+    }
+
+}
